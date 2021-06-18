977373a2a5042b1edf92d35b19130bff05395765
Tina Iuso <tinaiuso@gmail.com>
2021-06-18 16:22:52 +0100

    attributes added to the schema

--- db/migrate/20210618143732_create_public_toilet_items.rb
+++ db/migrate/20210618143732_create_public_toilet_items.rb
@@ -1,8 +1,9 @@
 class CreatePublicToiletItems < ActiveRecord::Migration[6.0]
   def change
     create_table :public_toilet_items do |t|
-
-      t.timestamps
+      t.string :borough_name
+      t.string :street_name
+      t.string :postcode
     end
   end
 end
