0827228550c774a30e2c109992f84bd345dbb25a
Tina Iuso <tinaiuso@gmail.com>
2021-06-18 17:03:46 +0100

    defined index, show, create, update and destroy actions

--- app/controllers/public_toilet_items_controller.rb
+++ app/controllers/public_toilet_items_controller.rb
@@ -1,2 +1,44 @@
 class PublicToiletItemsController < ApplicationController
+
+# Index: display all instances of public toilet items in our database
+    def index
+      @publicToiletItems = PublicToiletItem.all
+      render json: @publicToiletItems
+    end  
+
+# Show: display an instance of a public toilet item
+    def show
+      @publicToiletItem = PublicToiletItem.find(params[:id])
+      render json: @publicToiletItem
+    end
+
+# Create: create an instance of a public toilet item
+    def create
+      @publicToiletItem = PublicToiletItem.create(
+        borough_name: params[:borough_name],
+        street_name: params[:street_name],
+        postcode: params[:postcode]
+      )
+      render json: @publicToiletItem 
+    end
+
+# Update: update an instance of an existing public toilet item
+    def update
+      @publicToiletItem = PublicToiletItem.find(params[:id])
+      @publicToiletItem.update(
+        borough_name: params[:borough_name],
+        street_name: params[:street_name],
+        postcode: params[:postcode]
+      )
+      render json: @publicToiletItem 
+    end
+
+# Delete: an instance of an existing public toilet item
+    def destroy
+      @publicToiletItems = PublicToiletItem.all
+      @publicToiletItem = PublicToiletItem.find(params[:id])
+      @publicToiletItem.destroy
+      render json: @publicToiletItems  
+    end
+
 end
