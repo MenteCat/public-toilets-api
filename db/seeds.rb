0bdf3175cc97188cb5c4b962b51afebf731ec375
Tina Iuso <tinaiuso@gmail.com>
2021-06-19 21:07:37 +0100

    seeded with instances of toilets

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+toilet1 = PublicToiletItem.create(borough_name: "Kingston Upon Thames", street_name: "Lully road", postcode: "KT1 1RQ")
+toilet2 = PublicToiletItem.create(borough_name: "Camden", street_name: "Magic road", postcode: "NW1 0GA")
+toilet3 = PublicToiletItem.create(borough_name: "Ealing", street_name: "Foo road", postcode: "W13 8BB")
